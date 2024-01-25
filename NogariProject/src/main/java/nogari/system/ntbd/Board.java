37023d54dcbc95709f98cda760bf031dff9967fc
Yuni <yseolcoding@gmail.com>
2024-01-25 16:12:08 +0900

    add Board

---
+++ NogariProject/src/main/java/nogari/system/ntbd/Board.java
@@ -0,0 +1,22 @@
+package nogari.system.ntbd;
+
+import java.sql.Date;
+
+public class Board {
+
+    private Long boardCd;
+    private String fileCd;
+    private String ntbdDiv;
+    private Long viewCnt;
+    private String topYn;
+    private String postNm;
+    private String postCont;
+    private String postInDate;
+    private String postOutDate;
+    private Date regDt;
+    private String regId;
+    private Date upddDt;
+    private String udpId;
+
+
+}
